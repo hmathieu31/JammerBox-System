f3429b15f17e00c3346b865e5456f889ebb938f9
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2022-03-29 17:05:15 +0200

    Testing web page

---
+++ Nouveau PIR/interface-web/src/Test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Test.css";
+
+export default function Test() {
+  return (
+    <div className="desktop-1 flex-col-hstart-vstart clip-contents">
+      <div className="group-374 flex-col">
+        <div className="frame-5 flex-col-hcenter-vstart">
+          <p className="txt-941">PIR Bail</p>
+        </div>
+        <div className="group-587">
+          <p className="txt-831 flex-hcenter">TEST 1</p>
+        </div>
+        <div className="group-471">
+          <p className="txt-831 flex-hcenter">TEST 2</p>
+        </div>
+        <div className="group-10810">
+          <p className="txt-831 flex-hcenter">TEST 3</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- Nouveau PIR/interface-web/src/index.js
+++ Nouveau PIR/interface-web/src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-//import Lol from "./App";
+//import App from "./App";
+//import Test from "./Test";
 import reportWebVitals from "./reportWebVitals";
+import Test from "./Test";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name="lol" />
+    <Test />
   </React.StrictMode>,
   document.getElementById("root")
 );
