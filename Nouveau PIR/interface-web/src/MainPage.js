be6c9e219f2f73580a68908ccb857b6cececb131
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2022-04-06 10:07:34 +0200

    MainPage component test

---
+++ Nouveau PIR/interface-web/src/MainPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import MainPage from "./MainPage";
+
+export default function MainPage() {
+  return (
+    <div className="options-group">
+      <div className="group-5710 flex-col">
+        <div className="group-6">
+          <p className="txt-689 flex-hcenter">Synthesize tests</p>
+        </div>
+        <div className="group-7">
+          <p className="txt-673 flex-hcenter">Test historics</p>
+        </div>
+      </div>
+    </div>
+  );
+}
