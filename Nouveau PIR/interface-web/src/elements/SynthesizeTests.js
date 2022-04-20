ff6d73c7b7a29854720820b26b71dd48b87948e3
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2022-04-20 09:25:39 +0200

    Popup modal created

--- Nouveau PIR/interface-web/src/elements/SynthesizeTests.js
+++ Nouveau PIR/interface-web/src/elements/SynthesizeTests.js
@@ -9,7 +9,9 @@ function SynthesizeTests() {
       <div className="frame-1">
         <p className="txt-271 flex-hcenter">Synthesize Tests</p>
         {ReturnButton()}
-        <div>{ButtonList()}</div>
+        <div>
+          <ButtonList />
+        </div>
       </div>
     </>
   );
