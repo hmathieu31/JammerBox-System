99faa425aecbf51ca9f132e4c2e04d3a9460eb40
ToTTToR <vikinor007@gmail.com>
2022-04-21 23:13:31 +0200

    Popup pimp

--- Nouveau PIR/interface-web/src/components/ParameterPopup.js
+++ Nouveau PIR/interface-web/src/components/ParameterPopup.js
@@ -22,17 +22,17 @@ export default function ParameterPopup(truc) {
       <div className="test-configuration flex-col-hstart-vstart clip-contents">
         <div className="group-917 flex-col-hcenter">
           <p className="txt-120 flex-hcenter">Nom du test</p>
-          <div className="group-213 flex-col-hend">
+          <div className="flex-col-hend">
             <div className="group-21">
-              <p className="txt-649 flex-hcenter">Parametre :</p>
+              <p className="txt-649">Parametre :</p>
               <input className="rectangle-17 txt-905" />
             </div>
             <div className="group-786 flex-row">
-              <button className="group-6">
-                <p className="txt-905 flex-hcenter">Let’s not go</p>
+              <button className="buttonPopup">
+                <p className="txt-905">Let’s not go</p>
               </button>
-              <button className="group-6">
-                <p className="txt-637 flex-hcenter">Let’s go</p>
+              <button className="buttonPopup">
+                <p className="txt-637">Let’s go</p>
               </button>
             </div>
           </div>
