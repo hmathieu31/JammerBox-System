fe7c5ca5c6cfbed8f21eea75cbc09d236ea81425
Emmanuel Pastor <ommanu13@hotmail.fr>
2022-04-22 15:28:38 +0200

    Change Timer used to their new equivalents on STM32 for init functions

--- Nouveau PIR/firmware/source/timer.c
+++ Nouveau PIR/firmware/source/timer.c
@@ -30,21 +30,21 @@
 		// Fcy: 36,85 MHz
 		// 36,85 Mhz/ 64 = 575,78 kHz = 1,7367744624683038660599534544444�s
 
-		RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // Enable APB clock for the Timer2
-		TIM_Cmd(TIM2, DISABLE); // Disable Timer2
+		RCC->APB2ENR |= RCC_APB2ENR_TIM1EN; // Enable APB clock for the Timer1
+		TIM_Cmd(TIM1, DISABLE); // Disable Timer1
 		//No idle mode handling necessary on STM32
-		TIM_ITConfig(TIM2, TIM_IT_Trigger, DISABLE); // Disable Trigger Interrupt (called Gated Timer mode on Microchip)
-		TIM_InternalClockConfig(TIM2); // Tell the STM32 to use the internal clock (ticking at 72MHz)
-		TIM_SetCounter(TIM2, 0); // Clear Timer2 counter
+		TIM_ITConfig(TIM1, TIM_IT_Trigger, DISABLE); // Disable Trigger Interrupt (called Gated Timer mode on Microchip)
+		TIM_InternalClockConfig(TIM1); // Tell the STM32 to use the internal clock (ticking at 72MHz)
+		TIM_SetCounter(TIM1, 0); // Clear Timer1 counter
 		// TIM_period = (1/72MHz) * (PSC+1) *(ARR+1)
 		// (1/72MHz) * (119+1) * (62999+1) = 105ms
-		TIM_PrescalerConfig(TIM2, 119, TIM_PSCReloadMode_Immediate);
-		TIM_SetAutoreload(TIM2, 62999);
+		TIM_PrescalerConfig(TIM1, 119, TIM_PSCReloadMode_Immediate);
+		TIM_SetAutoreload(TIM1, 62999);
 
-		NIVC_SetPriority(TIM2_IRQn, 2); // Set Timer2 TIM2_IRQn Interrupt Priority Level
-		TIM_ClearFlag(TIM2, TIM_FLAG_Update); // Clear Timer2 Interrupt Flag
-		TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // Enable Timer2 interrupt
-		TIM_Cmd(TIM2, ENABLE); // Start Timer
+		NIVC_SetPriority(TIM1_UP_IRQn, 2); // Set Timer1 TIM2_IRQn Interrupt Priority Level
+		TIM_ClearFlag(TIM1, TIM_FLAG_Update); // Clear Timer1 Interrupt Flag
+		TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE); // Enable Timer1 interrupt
+		TIM_Cmd(TIM1, ENABLE); // Start Timer
 	}
 
 
@@ -56,21 +56,21 @@
 		// Fcy: 36,85 MHz
 		// 36,85 Mhz/ 256 = 143,95 kHz = 6.946 �s
 
-		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; // Enable APB clock for the Timer3
-		TIM_Cmd(TIM3, DISABLE); // Disable Timer3
+		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; // Enable APB clock for the Timer2
+		TIM_Cmd(TIM2, DISABLE); // Disable Timer2
 		//No idle mode handling necessary on STM32
-		TIM_ITConfig(TIM3, TIM_IT_Trigger, DISABLE); // Disable Trigger Interrupt (called Gated Timer mode on Microchip)
-		TIM_InternalClockConfig(TIM3); // Tell the STM32 to use the internal clock (ticking at 72MHz)
-		TIM_SetCounter(TIM3, 0); // Clear Timer3 counter
+		TIM_ITConfig(TIM2, TIM_IT_Trigger, DISABLE); // Disable Trigger Interrupt (called Gated Timer mode on Microchip)
+		TIM_InternalClockConfig(TIM2); // Tell the STM32 to use the internal clock (ticking at 72MHz)
+		TIM_SetCounter(TIM2, 0); // Clear Timer2 counter
 		// TIM_period = (1/72MHz) * (PSC+1) *(ARR+1)
 		// (1/72MHz) * (479+1) * (62999+1) = 420ms
-		TIM_PrescalerConfig(TIM3, 479, TIM_PSCReloadMode_Immediate);
-		TIM_SetAutoreload(TIM3, 62999);
+		TIM_PrescalerConfig(TIM2, 479, TIM_PSCReloadMode_Immediate);
+		TIM_SetAutoreload(TIM2, 62999);
 
-		NIVC_SetPriority(TIM3_IRQn, 2); // Set Timer3 TIM3_IRQn Interrupt Priority Level
-		TIM_ClearFlag(TIM3, TIM_FLAG_Update); // Clear Timer3 Interrupt Flag
-		TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE); // Enable Timer3 interrupt
-		TIM_Cmd(TIM3, ENABLE); // Start Timer
+		NIVC_SetPriority(TIM2_IRQn, 2); // Set Timer2 TIM2_IRQn Interrupt Priority Level
+		TIM_ClearFlag(TIM2, TIM_FLAG_Update); // Clear Timer2 Interrupt Flag
+		TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // Enable Timer2 interrupt
+		TIM_Cmd(TIM2, ENABLE); // Start Timer
 	}
 	
 	// ## Timer6 Init **CAM_PER/CRK_TOOTH_PER(start-value)//CRK_SHO_LEVEL pulse duration**
@@ -81,20 +81,20 @@
 		// Fcy: 36,85 MHz
 		// 36,85 Mhz/ 8 = 4606 kHz = 0.217 �s
 
-		RCC->APB1ENR |= RCC_APB1ENR_TIM6EN; // Enable APB clock for the Timer6
-		TIM_Cmd(TIM6, DISABLE); // Disable Timer6
+		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; // Enable APB clock for the Timer3
+		TIM_Cmd(TIM3, DISABLE); // Disable Timer3
 		//No idle mode handling necessary on STM32
-		TIM_ITConfig(TIM6, TIM_IT_Trigger, DISABLE); // Disable Trigger Interrupt (called Gated Timer mode on Microchip)
-		TIM_InternalClockConfig(TIM6); // Tell the STM32 to use the internal clock (ticking at 72MHz)
-		TIM_SetCounter(TIM6, 0); // Clear Timer6 counter
+		TIM_ITConfig(TIM3, TIM_IT_Trigger, DISABLE); // Disable Trigger Interrupt (called Gated Timer mode on Microchip)
+		TIM_InternalClockConfig(TIM3); // Tell the STM32 to use the internal clock (ticking at 72MHz)
+		TIM_SetCounter(TIM3, 0); // Clear Timer3 counter
 		// TIM_period = (1/72MHz) * (PSC+1) *(ARR+1)
 		// (1/72MHz) * (15+1) * (62999+1) = 14ms
-		TIM_PrescalerConfig(TIM6, 15, TIM_PSCReloadMode_Immediate);
-		TIM_SetAutoreload(TIM6, 62999);
+		TIM_PrescalerConfig(TIM3, 15, TIM_PSCReloadMode_Immediate);
+		TIM_SetAutoreload(TIM3, 62999);
 
-		NIVC_SetPriority(TIM6_IRQn, 2); // Set Timer6 TIM6_IRQn Interrupt Priority Level
-		TIM_ClearFlag(TIM6, TIM_FLAG_Update); // Clear Timer6 Interrupt Flag
-		TIM_ITConfig(TIM6, TIM_IT_Update, ENABLE); // Enable Timer6 interrupt
+		NIVC_SetPriority(TIM6_IRQn, 2); // Set Timer3 TIM6_IRQn Interrupt Priority Level
+		TIM_ClearFlag(TIM3, TIM_FLAG_Update); // Clear Timer3 Interrupt Flag
+		TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE); // Enable Timer3 interrupt
 	}
 
 	// ## Timer7 Init **Prescaler: 8; CAM_PER/CRK_TOOTH_PER(pulse duration)**
