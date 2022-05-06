ee79aad973148c9a8b66d07d569017cc07b5c820
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2022-05-06 18:33:33 +0200

    Cleaning my room

--- Nouveau PIR/interface-web/src/components/ButtonList.js
+++ Nouveau PIR/interface-web/src/components/ButtonList.js
@@ -1,67 +1,9 @@
 import "../CSS/Background.css";
 import "../CSS/MainPage.css";
 import "../CSS/ParameterPopup.css";
+import ParameterPopup from "./ParameterPopup";
 import React from "react";
 import Modal from "react-modal";
-
-class ParameterPopup extends React.Component {
-  options = [
-    {
-      label: "Ground",
-      value: "ground",
-    },
-    {
-      label: "Battery",
-      value: "battery",
-    },
-  ];
-
-  render() {
-    const { handleOpenClose } = this.props;
-    return (
-      <div className="test-configuration flex-col-hstart-vstart clip-contents">
-        <div className="group-917 flex-col-hcenter">
-          <p className="txt-120 flex-hcenter">{this.props.testName}</p>
-          <div className="group-213 flex-col-hend">
-            <div className="group-21">
-              <p className="txt-649 flex-hcenter">{this.props.testParam} :</p>
-              {this.props.isSelect ? (
-                <select
-                  onChange={this.props.handleChange}
-                  className="rectangle-17 txt-905 flex-hcenter"
-                >
-                  <option value="none" selected disabled hidden>
-                    Select option
-                  </option>
-                  {this.options.map((option) => (
-                    <option value={option.value}>{option.label}</option>
-                  ))}
-                </select>
-              ) : (
-                <div>
-                  <input
-                    type="number"
-                    pattern="[0-9]*"
-                    className="rectangle-17 txt-905 flex-hcenter"
-                    onChange={this.props.handleChange}
-                  />
-                </div>
-              )}
-            </div>
-            <div className="group-786 flex-row">
-              <button onClick={handleOpenClose} className="group-6">
-                <p className="txt-637 flex-hcenter">Cancel</p>
-              </button>
-              <button onClick={this.props.handleRun} className="group-6">
-                <p className="txt-637 flex-hcenter">Run Test</p>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
 
 class ButtonAttributes {
   constructor(name, hasparam, paramsTab, IsSelect) {
