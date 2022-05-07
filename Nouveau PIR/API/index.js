5937f1e913bf7beaf2a83994966b812703882204
ToTTToR <vikinor007@gmail.com>
2022-05-07 18:44:41 +0200

    Small changes

--- Nouveau PIR/API/index.js
+++ Nouveau PIR/API/index.js
@@ -1,15 +1,24 @@
 const usb = require("usb");
 const express = require("express");
 const app = express();
-const exemple = require("./exemple.json");
 const shell = require("shelljs");
 const bp = require("body-parser");
 const cors = require("cors");
 const fs = require("fs");
 
+var allowedOrigins = ["http://localhost:3000"];
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   })
 );
 
@@ -24,11 +33,10 @@ app.post("/run", (req, res) => {
     var param2 = req.body.TestParameter;
     var param3 = req.body.TestValue;
     shell.exec("../script_exemple.sh " + param1 + " " + param2 + " " + param3);
-    res.status(200).json(exemple);
+    res.status(200).json();
   }
 });
 
 app.listen(8080, () => {
   console.log("Serveur à l'écoute");
-  shell.exec("touch /Volumes/KINGSTON/test.json");
 });
