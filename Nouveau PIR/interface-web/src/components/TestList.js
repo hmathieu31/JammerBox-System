55c6d3152cd87291f089f3ffe63c209724513fc1
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2022-05-13 17:05:30 +0200

    Changing API adress
    Configuration working and message protocol sending working great
    
    Writing on USB file test result
    
    Adds test done to historicdata json, everything seems to work  fine
    
    Small change to test on the pir
    
    Some changes on CSS and historic data json
    
    Small changes in API and pythonscript

--- Nouveau PIR/interface-web/src/components/TestList.js
+++ Nouveau PIR/interface-web/src/components/TestList.js
@@ -1,29 +1,28 @@
 import "../CSS/Background.css";
 import "../CSS/MainPage.css";
+import "../CSS/Historics.css";
 import React from "react";
-import POPOSSpace from './ShowTestDiv';
+import POPOSSpace from "./ShowTestDiv";
 
-export default function Testing () {
-
-  const jsonData = require('../historicData.json');
+export default function Testing() {
+  const jsonData = require("../historicData.json");
   console.log(jsonData);
+
+  //console.log(Object.entries(jsonData));
 
   const spaces = Object.entries(jsonData).map((obj) => {
     var newObj = obj[1];
     console.log(newObj.id);
-  return(  
-    <POPOSSpace
-    id={newObj.id}
-    test_name={newObj.test_name}
-    date={newObj.date}
-    parametre={newObj.parametre}
-    result={newObj.result}
-    />
-  )})
-
     return (
-        <div className="POPOSList">
-          { spaces }
-        </div>
-    )
+      <POPOSSpace
+        id={newObj.id}
+        test_name={newObj.test_name}
+        date={newObj.date}
+        parametre={newObj.parametre}
+        result={newObj.result}
+      />
+    );
+  });
+
+  return <div className="POPOSList">{spaces}</div>;
 }
