fa21242e4ea363d3dc8005a7f5625767acafc93c
Tsjerfell <67692265+Tsjerfell@users.noreply.github.com>
2022-05-24 16:54:11 +0200

    Changed IC to EXTI everywhere (#112)

--- Nouveau PIR/firmware/source/exti.c
+++ Nouveau PIR/firmware/source/exti.c
@@ -7,12 +7,12 @@
 /*****************************************************************************/
 
 // ### Basic includes ###
-	#include "ic.h"
+	#include "exti.h"
 	#include "stm32f10x.h"
 	#include "stm32f10x_exti.h"
 // ### Functions ###
 
-	// ## Input capture 1 Init  ** CRK ** rising edge
+	// ## EXTI8 Init  ** CRK ** rising edge
 	void EXTI8Init(void){
 		EXTI_InitTypeDef EXTI_Struct;
 		EXTI_StructInit(&EXTI_Struct);
@@ -23,7 +23,7 @@
 		EXTI_Init(&EXTI_Struct);
 	}
 
-	// ## Input capture 2 Init ** CRK ** falling edge 
+	// ## EXTI9 Init ** CRK ** falling edge 
 	void EXTI9Init(void){
 		EXTI_InitTypeDef EXTI_Struct;
 		EXTI_StructInit(&EXTI_Struct);
@@ -34,18 +34,7 @@
 		EXTI_Init(&EXTI_Struct);
 	}
 
-	// ## Input capture 2 Init  ** CAM1 ** rising edge
-	void EXTI14Init(void){
-		EXTI_InitTypeDef EXTI_Struct;
-		EXTI_StructInit(&EXTI_Struct);
-		EXTI_Struct.EXTI_Line=EXTI_Line14;
-		EXTI_Struct.EXTI_Mode=EXTI_Mode_Interrupt;
-		EXTI_Struct.EXTI_Trigger=EXTI_Trigger_Rising;
-		EXTI_Struct.EXTI_LineCmd=ENABLE;
-		EXTI_Init(&EXTI_Struct);
-	}
-
-	// ## Input capture 4 Init ** CAM1 ** falling edge 
+	// ## EXTI11 Init ** CAM1 ** falling edge 
 	void EXTI11Init(void){
 		EXTI_InitTypeDef EXTI_Struct;
 		EXTI_StructInit(&EXTI_Struct);
@@ -56,7 +45,7 @@
 		EXTI_Init(&EXTI_Struct);
 	}
     
-	// ## Input capture 5 Init ** CAM2 ** rising edge 
+	// ## EXTI12 Init ** CAM2 ** rising edge 
 	void EXTI12Init(void){
 		EXTI_InitTypeDef EXTI_Struct;
 		EXTI_StructInit(&EXTI_Struct);
@@ -67,7 +56,7 @@
 		EXTI_Init(&EXTI_Struct);		
 	}
 
-	// ## Input capture 6 Init ** CAM2 ** falling edge 
+	// ## EXTI13 Init ** CAM2 ** falling edge 
 	void EXTI13Init(void){
 		EXTI_InitTypeDef EXTI_Struct;
 		EXTI_StructInit(&EXTI_Struct);
@@ -76,7 +65,18 @@
 		EXTI_Struct.EXTI_Trigger=EXTI_Trigger_Falling;
 		EXTI_Struct.EXTI_LineCmd=ENABLE;
 		EXTI_Init(&EXTI_Struct);		
-	}    
+	} 
+
+	// ## EXTI14 Init  ** CAM1 ** rising edge
+	void EXTI14Init(void){
+		EXTI_InitTypeDef EXTI_Struct;
+		EXTI_StructInit(&EXTI_Struct);
+		EXTI_Struct.EXTI_Line=EXTI_Line14;
+		EXTI_Struct.EXTI_Mode=EXTI_Mode_Interrupt;
+		EXTI_Struct.EXTI_Trigger=EXTI_Trigger_Rising;
+		EXTI_Struct.EXTI_LineCmd=ENABLE;
+		EXTI_Init(&EXTI_Struct);
+	}   
 	
 /*****************************************************************************/
 /*****************************************************************************/
