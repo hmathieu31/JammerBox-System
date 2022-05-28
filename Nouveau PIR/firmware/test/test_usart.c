39314794c446ef1e2ccd3290683d0020e88863e5
ToTTToR <vikinor007@gmail.com>
2022-05-28 23:50:52 +0200

    Transmit et receive non bloquant

--- Nouveau PIR/firmware/test/test_usart.c
+++ Nouveau PIR/firmware/test/test_usart.c
@@ -2,16 +2,18 @@
 #include "usart.h"
 #include "stm32f10x.h"
 #include "stm32f10x_usart.h"
+#define BUFFER_SIZE 4
 
 int main(void){
     
     MX_USART1_Init(void);
+    HAL_USART_IRQHandler(&husart1);
 
     while(1){
-        uint8_t buffer[4];
-        switch(HAL_USART_Receive(husart1,&buffer,4,1000)){
+        uint8_t bufferReceive[BUFFER_SIZE];
+        uint8_t bufferTransmit[BUFFER_SIZE];
+        switch(HAL_USART_TransmitReceive_IT(husart1,&bufferTransmit,&bufferReceive,BUFFER_SIZE,1000)){
             case HAL_OK:
-                HAL_USART_Transmit(husart1,&buffer,4,1000);
                 break;
             case HAL_ERROR:
                 break;
