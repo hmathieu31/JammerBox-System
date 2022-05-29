e12a69d16aa9e942beb1dec0aa4c9153ad51e1fa
ToTTToR <vikinor007@gmail.com>
2022-05-29 21:15:32 +0200

    Configuration for installing node automatically

--- Nouveau PIR/Configure.sh
+++ Nouveau PIR/Configure.sh
@@ -11,6 +11,13 @@ API_FOLDER="./API"
 
 echo "{\"IP_ADDRESS\":\"$1\"}" > $WEB_FOLDER/src/RaspiProp.json
 
+sudo su
+curl -fsSL https://deb.nodesource.com/setup_17.x | bash -
+sudo apt install nodejs
+
+node --version
+npm --version
+
 npm --prefix "$WEB_FOLDER" install "$WEB_FOLDER"
 npm --prefix "$API_FOLDER" install "$API_FOLDER"
 pip install pyserial 
