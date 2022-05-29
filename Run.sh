7ada3b27a92b266bd6b7a4f7a54ae8887f2825ba
ToTTToR <vikinor007@gmail.com>
2022-05-30 01:24:43 +0200

    Remove unused files

--- Run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PIR_FOLDER=./Nouveau\ PIR
-WEB_FOLDER=PIR_FOLDER/interface-web
-API_FOLDER=PIR_FOLDER/API
-
-#Running the API
-node API_FOLDER/index.js &
-npm --prefix WEB_FOLDER start 
